9c5deeed974834f09c03b92a51b65a0723645a3a
bigsimp <simplyn64@gmail.com>
2019-10-14 12:46:23 -0500

    extract canvas zip started

--- grading_lib/zip.py
+++ grading_lib/zip.py
@@ -67,5 +67,48 @@ def extract_canvas_zip(zippath, outpath, tmpdir, roster: Roster, internal_tarbal
     ensure_dir_exists(outpath)
 
     for file in os.listdir(zipdir):
-        print(file)
-        # TODO: Write me
+        zipdir = os.path.join(tmpdir, "zip")
+        extract_zip(zippath, zipdir)
+        ensure_dir_exists(outpath)
+
+        for subdir in os.listdir(zipdir):
+            # ext_id from canvas is listed second if not late, otherwise listed third
+            # either fnamelname_extid_whatever or fnamelname_LATE_extid_whatever
+            if "LATE" in subdir:
+                ext_id = subdir.split("_")[2]
+            else:
+                ext_id = subdir.split("_")[1]
+
+            sid = roster.get_student_id_by_external_id(ext_id)
+
+            if sid is None:
+                continue
+            # assert sid is not None, "Unknown student"
+
+            if internal_tarball:
+                # TODO: catch exceptions and give good error.
+                tarpath = os.path.join(zipdir, subdir)
+                if os.path.isdir(tarpath):
+                    tarball_name = os.listdir(os.path.join(zipdir, subdir))[0]
+                    tarpath = os.path.join(zipdir, subdir, tarball_name)
+
+                #            if not tarpath.endswith(".tar.gz"):
+                #                print("Student submission must be a tar.gz. filename was {}".format(tarpath))
+                #                continue
+                sopath = os.path.join(outpath, sid)
+                try:
+                    with tarfile.open(tarpath, mode="r:*") as tf:
+                        tf.extractall(sopath)
+                except tarfile.ReadError:
+                    print("Submission was not a valid tarball.")
+                print(sid)
+            else:
+                submission_name = os.listdir(os.path.join(zipdir, subdir))[0]
+                submissionpath = os.path.join(zipdir, subdir, submission_name)
+
+                ext = ""
+                if "." in submission_name:
+                    ext = submission_name[submission_name.index("."):]
+                new_path = os.path.join(outpath, sid + ext)
+
+                copyfile(submissionpath, new_path)
